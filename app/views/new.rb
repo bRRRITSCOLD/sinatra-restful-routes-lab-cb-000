5aad591b1de20e0673b31216585cf491e56a425c
Blaine Richardson <blainerrichardson@gmail.com>
2017-10-06 03:31:26 +0000

    automatically backed up by learn

--- app/views/new.rb
+++
@@ -1,6 +0,0 @@
-<form action="/recipes" method="post">
-  <p>Name: <input type="text" name="name"></p>
-  <p>Ingredients: <input type="text" name="content"></p>
-  <p>Cook Time: <input type="text" name="content"></p>
-  <p><input type="submit" id="submit"></p>
-</form>
