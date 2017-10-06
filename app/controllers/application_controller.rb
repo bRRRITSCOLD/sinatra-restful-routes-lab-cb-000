3f4a36e8444b157191797961c79378417c2d8be8
Blaine Richardson <blainerrichardson@gmail.com>
2017-10-06 03:01:47 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,10 @@
+
 class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
   end
-  get '/' do
+  getas '/' do
     erb :index
   end
 end
